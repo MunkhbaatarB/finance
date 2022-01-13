3cc42b246f7d1691608b3abbd78df87171fe3eae
Mos <b.munkhbaatar10@gmail.com>
2022-01-13 18:31:57 +0800

    Bvh uil ajilgaag shineer ehlvvleh punkts

--- app.js
+++ app.js
@@ -1,27 +1,55 @@
 // Дэлгэцтэй ажиллах контроллер
-var uiController = (function() {})();
+var uiController = (function() {
+
+      var DOMstrings = {
+            inputType: ".add__type",
+            inputDescription: '.add__description',
+            inputValue: '.add__value',
+            addBtn: '.add__btn'
+      }
+
+      return{
+            getInput: function(){
+                  return{
+                        type: document.querySelector(DOMstrings.inputType).value,
+                        description: document.querySelector(DOMstrings.inputDescription).value,
+                        value: document.querySelector(DOMstrings.inputValue).value
+                  }
+            },
+      getDomstrings: function(){
+            return DOMstrings;
+      }
+      }
+})();
 
 // Санхүүтэй ажжиллах контроллер
 var financeController = (function() {})();
 
 // Програмын холбогч контроллер
 var appController = (function(uiController, financeController) {
-  var ctrlAddItem = function() {
+
+      var DOM = uiController.getDomstrings();
+
+      var ctrlAddItem = function() {
       // 1.Оруулах өгөгдлийг дэлгэцээс олж авна.
-    console.log("Дэлгэцээс өгөгдөлөө авах хэсэг");
+     console.log(uiController.getInput())
       // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
       // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгтнь гаргана.
       // 4. Төсвийг тооцоолно
       // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
   };
+  var setupEventListeners = function(){
+      var DOM = uiController.getDomstrings();
 
-  document.querySelector(".add__btn").addEventListener("click", function() {
-    ctrlAddItem();
-  });
 
-  document.addEventListener("keypress", function(event) {
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
-    }
-  });
+      document.querySelector(DOM.addBtn).addEventListener("click", function() {
+            ctrlAddItem();
+          });
+        
+          document.addEventListener("keypress", function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+              ctrlAddItem();
+            }
+          });
+  }
 })(uiController, financeController);
